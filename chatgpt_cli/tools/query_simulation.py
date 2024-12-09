a37c9136382c85e7c46fa46cb4a5a866763e8d06
Mostafa Abdelrashied <mostafa.abdelrashied@outlook.de>
2024-12-09 01:39:46 +0100

    fix names

--- chatgpt_cli/tools/query_simulation.py
+++ chatgpt_cli/tools/query_simulation.py
@@ -3,15 +3,15 @@ from sqlalchemy import text
 from chatgpt_cli.db.session import get_session
 
 
-async def query_db(query: str):
+async def query_simulation_table(query: str):
     async with get_session() as session:
         result = await session.execute(text(query))
         rows = result.fetchall()
         return {"query": query, "result": rows}
 
 
-QUERY_DB_TOOL = {
-    "name": "query_db",
+QUERY_SIMULATION_TABLE_TOOL = {
+    "name": "query_simulation_table",
     "description": "Query the database for information about simulation data. The table exists under schema 'optimization' and table name 'simulation'",
     "parameters": {
         "type": "object",
--- chatgpt_cli/tools/tool_manager.py
+++ chatgpt_cli/tools/tool_manager.py
@@ -1,7 +1,8 @@
-from chatgpt_cli.tools.query_db import QUERY_DB_TOOL, query_db
+from chatgpt_cli.tools.query_simulation import (
+    QUERY_SIMULATION_TABLE_TOOL,
+    query_simulation_table,
+)
 
-TOOLS = [QUERY_DB_TOOL]
+TOOLS = [QUERY_SIMULATION_TABLE_TOOL]
 # A dictionary mapping tool name to the execution function
-TOOL_FUNCTIONS = {
-    "query_db": query_db,
-}
+TOOL_FUNCTIONS = {"query_simulation_table": query_simulation_table}
